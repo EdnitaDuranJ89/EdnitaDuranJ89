ac06484556daa3cc62936df8ca801dd38a4c6c60
ednitaduranj <ednitaduranj@gmail.com>
2021-10-24 22:51:14 -0500

    reto4

---
+++ reto4/src/main/java/co/usa/ciclo3/ciclo3/repository/ReservationRepository.java
@@ -0,0 +1,34 @@
+package co.usa.ciclo3.ciclo3.repository;
+
+import co.usa.ciclo3.ciclo3.modelo.Motorbike;
+import co.usa.ciclo3.ciclo3.modelo.Reservation;
+import co.usa.ciclo3.ciclo3.modelo.ReservationCrud;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+
+
+@Repository
+public class ReservationRepository {
+    
+    @Autowired
+    private ReservationCrud reservationCrudRepository;
+    
+    public List<Reservation> getAll(){
+        return (List<Reservation>) reservationCrudRepository.findAll();
+    }
+    
+    public Optional<Reservation> getReservation(int id){
+        return reservationCrudRepository.findById(id);
+    }
+    
+    public Reservation save(Reservation reservation){
+        return reservationCrudRepository.save(reservation);
+    }
+    
+    public void delete(Reservation reservation){
+        reservationCrudRepository.delete(reservation);
+    }
+}
